d7e2b5896ccc7b64a738df815ab4aaccc01b0917
Yaroslav <sych521@gmail.com>
2023-07-02 09:55:19 +0300

    Little fix

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSearchParams, Link, useLocation } from 'react-router-dom';
 import { getMovies } from 'fetch';
 
